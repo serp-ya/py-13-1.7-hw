68791335cb929a1a1e2f34ec0d85618fc3820e44
Sergey G. Kundryukov <skundryukov@tsc.ts>
2018-02-15 12:20:27 +0300

    Добавлен общий класс всех Животных

---
+++ Animal.py
@@ -0,0 +1,23 @@
+class Animals:
+    it_alive = True
+    has_brain = True
+    has_heart = True
+    has_nervers = True
+    weight = None       # kilograms
+
+    def feed(self, food):
+        self.eat(food)
+
+    def eat(self, food):
+        callories = ((food['protein'] * 4) + (food['carbohydrate'] * 4) + (food['fat'] * 9))
+        self.digestion(callories, food.poison)
+
+    def digestion(self, callories, poison):
+        if poison:
+            self.it_alive = False
+            print('I died')
+        else:
+            self.gain(callories)
+
+    def gain(self, callories):
+        self.weight += (callories / 1000) # (callories / 1000) for example
